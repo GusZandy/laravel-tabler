6745504ced471c3964f5abda3534f8f98e2d40bc
Farid Inawan <frdteknikelektro@gmail.com>
2018-07-10 20:42:09 +0700

    Make it laravel-mix friendly

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,14 +11,26 @@ let mix = require('laravel-mix');
  |
  */
 
- mix.js('resources/assets/js/tabler.js', 'public/js')
-    .sass('resources/assets/sass/tabler.scss', 'public/css');
+mix.webpackConfig({
+  resolve: {
+    alias: {
+      'circle-progress': path.resolve(__dirname, 'node_modules/tabler-ui/dist/assets/js/vendors/circle-progress.min.js'),
+      'core': path.resolve(__dirname, 'node_modules/tabler-ui/dist/assets/js/core.js')
+    }
+  }
+});
 
- mix.autoload({
-     jquery: [ '$', 'jQuery', 'jquery'],
- });
+mix.js('resources/assets/js/tabler.js', 'public/js')
+  .sass('resources/assets/sass/tabler.scss', 'public/css');
 
- mix.extract([
-     'lodash', 'jquery', 'vue'
- ], 'public/js/vendor.js');
- mix.version();
+mix.autoload({
+  jquery: [ '$', 'jQuery', 'jquery', 'window.jQuery'],
+});
+
+mix.extract([
+  'lodash', 'jquery', 'vue'
+], 'public/js/vendor.js');
+
+mix.version();
+
+mix.setPublicPath('public');
